3e5ca94af765c0cb3b9a0637f41a62e4b9340ae9
Prasanth Kolachina <prasanthk.ms09@gmail.com>
2018-07-18 21:58:22 +0200

    v1 of align_pud. only produces reordered sequences

--- src/align_pud.py
+++ src/align_pud.py
@@ -1,23 +1,52 @@
 #!/usr/bin/env python3
 
-def align_treeset(*trees, traversal=None) : 
-  tree_seq = {} ;
+from __future__ import print_function, division ; 
+
+from functools import partial ; 
+from operator import itemgetter ; 
+import os.path ; 
+import re ; 
+import sys ; 
+
+import treeops as AT ; 
+import utils ; 
+import conll_utils as cu ; 
+
+def align_treeset(trees, traversal=None, langs=None) :
+  ctrees    = map(itemgetter(1), trees) ; 
+  tree_seqs = [] ;
   if not traversal : 
-    traversal = Tree.preorder ; 
-
-  for lidx, tree in enumerate(trees, start=1) : 
-    tree_seqs[lidx] = traversal(tree) ; 
-
-  return multiLcs(tree_seqs) ;
+    traversal = AT.preorder ; 
     
+  for lidx, tree in enumerate(ctrees, start=1) : 
+    tree_seqs.append( traversal(tree) ) ;
+  print("="*100) ;
+  for l, seq in zip(langs, tree_seqs) :
+    print("{0}\t{1}".format(l, ' '.join(seq))) ;
+  print("="*100) ; 
+  print("\n\n")  ; 
+
+  pairsc = len(trees)*(len(trees)-1) // 2 ;
+  distm  = [0 for _ in range(pairsc)] ; 
+
+  #for tpair in it.combinations(tree_seqs, 2) :
+  #  distm.append( lcs(tpair[0], tpair[1]) ) ;
+
+  return distm ; 
 
 def align_treebank(*conllinps) :
-  treebank = {} ; 
-  for lidx, inp in enumerate(conllinps, start=1) : 
-    treebank[lidx] = [sent for sent in sentences_from_conll(inp) ] ;
+  cu.FIELDS = cu.CONLLU_COLUMNS ; 
+  treebanks = [None for _ in conllinps] ;
+  exLang    = lambda trname : re.findall('^([a-z_]+?)-', trname)[0] ;
+  langs     = list(map(exLang, [os.path.split(inp)[1] for inp in conllinps])) ;
+  for lidx, inp in enumerate(conllinps) : 
+    clines  = utils.lines_from_file(inp) ;
+    treebanks[lidx] = [sent for sent in cu.sentences_from_conll(clines) ] ;
+  paired_treebanks = list(zip(*treebanks)) ;
+  align_treeset_   = partial(align_treeset, langs=langs) ; 
+  alignments       = map(align_treeset_, paired_treebanks) ;
+  for align in alignments : 
+    pass ; 
 
-  paired_treebanks = zip(*treebank.values() ) ;
-  alignments = map(align_treeset, paired_treebanks) ;
-  return alignments ; 
-
-
+if __name__ == '__main__':
+  align_treebank(*sys.argv[1:]) ;
